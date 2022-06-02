8b7ba75bcf056ca9d410744264d1ba349a6087f3
nuri1126 <nuri1126@users.noreply.github.com>
2022-06-02 11:59:12 -0700

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import WebcamComponent from "./components/WebcamComponent";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <WebcamComponent />
+      <header>
+      <Navbar />
       </header>
+      <WebcamComponent />
     </div>
   );
 }

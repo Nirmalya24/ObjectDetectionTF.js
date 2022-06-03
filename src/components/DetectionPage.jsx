52ad213ca4a6a37a22f77ee4142ee3ece90ab88b
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-06-02 22:16:37 -0700

    Remove unwanted components

--- src/components/DetectionPage.jsx
+++ src/components/DetectionPage.jsx
@@ -1,5 +1,4 @@
 import Navbar from "./Navbar";
-import Alert from "./Alert";
 import CameraRender from "./CameraRenderer";
 
 const DetectionPage = () => {
@@ -7,8 +6,6 @@ const DetectionPage = () => {
     <div className="h-screen">
       <div className="container mx-auto px-4">
         <Navbar AppName="FindThatObject" />
-        <Alert message="Detection stuff to be shown here" type="info" />
-        <Alert message="No camera detected" type="error" />
         <CameraRender />
       </div>
     </div>

7237ea8e18b3cf95f87cb039613275a6b62e8e39
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-06-02 22:17:12 -0700

    Add padding to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 
 const Navbar = (props) => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 p-4">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex="0" className="btn btn-ghost lg:hidden">

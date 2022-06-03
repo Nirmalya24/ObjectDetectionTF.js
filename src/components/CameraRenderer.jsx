60916224bc131d6532596cd0fe86c1397d078670
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-06-02 22:16:21 -0700

    Detect if camera access was granted or not

--- src/components/CameraRenderer.jsx
+++ src/components/CameraRenderer.jsx
@@ -1,8 +1,49 @@
+import { useState, useRef, useEffect } from "react";
+import Alert from "./Alert";
 import CameraControls from "./CameraControls";
+
 const CameraRender = () => {
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then(() => {
+        setStreamAvailable(true);
+      })
+      .catch((err) => {
+        setStreamAvailable(false);
+      });
+  }, []);
+
+  const [streamAvailable, setStreamAvailable] = useState(false);
+  const videoRef = useRef(null);
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.play();
+        setStreamAvailable(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setStreamAvailable(false);
+      });
+  };
+
   return (
     <>
-      <CameraControls />
+      {streamAvailable ? (
+        <>
+          <div className="border-4 border-sky-500 rounded-lg">
+            <video ref={videoRef} className="w-4/5 h-3/5" autoPlay />
+          </div>
+
+          <CameraControls />
+        </>
+      ) : (
+        <Alert message="No camera detected" type="error" />
+      )}
     </>
   );
 };

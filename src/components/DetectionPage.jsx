53456926630fd61dac931bb5351952ad82d42a6d
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-06-02 19:42:25 -0700

    WIP: Need to add functionality

--- src/components/DetectionPage.jsx
+++ src/components/DetectionPage.jsx
@@ -1,12 +1,15 @@
 import Navbar from "./Navbar";
 import Alert from "./Alert";
+import CameraRender from "./CameraRenderer";
 
 const DetectionPage = () => {
   return (
     <div className="h-screen">
-      <div class="container mx-auto px-4">
+      <div className="container mx-auto px-4">
         <Navbar AppName="FindThatObject" />
         <Alert message="Detection stuff to be shown here" type="info" />
+        <Alert message="No camera detected" type="error" />
+        <CameraRender />
       </div>
     </div>
   );

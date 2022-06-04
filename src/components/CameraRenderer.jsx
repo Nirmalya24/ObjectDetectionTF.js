10725cceb7c316efc6cfec61596ccce36c561d9d
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-06-03 18:59:23 -0700

    Simplify component

--- src/components/CameraRenderer.jsx
+++ src/components/CameraRenderer.jsx
@@ -1,51 +1,16 @@
-import { useState, useRef, useEffect } from "react";
-import Alert from "./Alert";
-import CameraControls from "./CameraControls";
+import { useRecoilValue } from "recoil";
+import CameraControls, { isCameraOpenState } from "./CameraControls";
+import Camera from "./Camera";
 
-const CameraRender = () => {
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then(() => {
-        setStreamAvailable(true);
-      })
-      .catch((err) => {
-        setStreamAvailable(false);
-      });
-  }, []);
-
-  const [streamAvailable, setStreamAvailable] = useState(false);
-  const videoRef = useRef(null);
-
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then((stream) => {
-        videoRef.current.srcObject = stream;
-        videoRef.play();
-        setStreamAvailable(true);
-      })
-      .catch((err) => {
-        console.log(err);
-        setStreamAvailable(false);
-      });
-  };
-
+const CameraRenderer = () => {
+  const isCameraOpen = useRecoilValue(isCameraOpenState);
   return (
-    <>
-      {streamAvailable ? (
-        <>
-          <div className="border-4 border-sky-500 rounded-lg">
-            <video ref={videoRef} className="w-4/5 h-3/5" autoPlay />
-          </div>
+    <div>
+      <div className="flex">{isCameraOpen && <Camera />}</div>
 
-          <CameraControls />
-        </>
-      ) : (
-        <Alert message="No camera detected" type="error" />
-      )}
-    </>
+      <CameraControls />
+    </div>
   );
 };
 
-export default CameraRender;
+export default CameraRenderer;

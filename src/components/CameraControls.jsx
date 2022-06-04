122318a247baea1ab2cca17356d9fe5db177c448
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-06-04 00:25:46 -0700

    Adding front/Back Camera toggle logic

--- src/components/CameraControls.jsx
+++ src/components/CameraControls.jsx
@@ -8,16 +8,33 @@ export const isCameraOpenState = atom({
   default: false,
 });
 
+export const facingModeState = atom({
+  key: "facingMode",
+  default: "environment",
+});
+
 const CameraControls = () => {
   const [isCameraOpen, setIsCameraOpen] = useRecoilState(isCameraOpenState);
+  const [facingMode, setFacingMode] = useRecoilState(facingModeState);
 
   const cameraOpenToggle = () => {
     setIsCameraOpen(!isCameraOpen);
     // TODO: Delete for production
     console.log("Camera toggle clicked. Current state: ", isCameraOpen);
   };
+
+  const cameraFacingModeToggle = () => {
+    if (isCameraOpen) {
+      if (facingMode === "environment") {
+        setFacingMode("user");
+      } else {
+        setFacingMode("environment");
+      }
+    }
+  };
+
   return (
-    <div className="flex w-2/4 mx-auto">
+    <div className="flex w-2/4 mx-auto mt-5">
       <div
         className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"
         onClick={() => setIsCameraOpen(!isCameraOpen)}
@@ -27,7 +44,10 @@ const CameraControls = () => {
       {isMobile && (
         <>
           <div className="divider divider-horizontal"></div>
-          <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
+          <div
+            className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"
+            onClick={cameraFacingModeToggle}
+          >
             <PhoneToggle />
           </div>
         </>

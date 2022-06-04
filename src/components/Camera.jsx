b62efb8353aa648ce1932ad37f2c365fc5da781b
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-06-04 03:10:02 -0700

    Camera component can do detections now

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Measure from "react-measure";
 import { useUserMedia } from "./hooks/useUserMedia";
 import { useCardRatio } from "./hooks/useCardRatio";
@@ -9,6 +9,10 @@ import { facingModeState } from "./CameraControls";
 import Alert from "./Alert";
 import Webcam from "react-webcam";
 import { isMobile } from "react-device-detect";
+// Tensoflow
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import { drawRect } from "../utilities";
 
 const CAPTURE_OPTIONS = {
   audio: false,
@@ -19,10 +23,19 @@ const Camera = () => {
   const canvasRef = useRef(null);
   const videoRef = useRef(null);
   const webcamRef = useRef(null);
+  useEffect(() => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState !== null
+    )
+      runCoco();
+  });
 
   const [container, setContainer] = useState({ width: 0, height: 0 });
   const facingMode = useRecoilValue(facingModeState);
   const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const [detectionLoop, setDetectionLoop] = useState(null);
 
   const videoConstraints = {
     facingMode: facingMode,
@@ -37,23 +50,76 @@ const Camera = () => {
     container.height
   );
 
-  if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
-    videoRef.current.srcObject = mediaStream;
-    console.log(videoRef.current);
-  }
+  // TODO: Code clean up
+  // if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
+  //   videoRef.current.srcObject = mediaStream;
+  //   console.log("VideoRef.current", videoRef.current);
+  // }
 
-  function handleResize(contentRect) {
+  const handleResize = (contentRect) => {
     setContainer({
       width: contentRect.bounds.width,
       height: Math.round(contentRect.bounds.width / aspectRatio),
     });
-  }
+  };
 
-  function handleCanPlay() {
-    calculateRatio(videoRef.current.videoHeight, videoRef.current.videoWidth);
-    setIsVideoPlaying(true);
-    videoRef.current.play();
-  }
+  // TODO: Code Clean up
+  // const handleCanPlay = () => {
+  //   calculateRatio(videoRef.current.videoHeight, videoRef.current.videoWidth);
+  //   setIsVideoPlaying(true);
+  //   videoRef.current.play();
+  // };
+
+  // Main Function
+  const runCoco = async () => {
+    // Load network
+    const net = await cocossd.load();
+    console.log("Loading network");
+
+    // Loop and detect hands
+    setDetectionLoop(
+      setInterval(() => {
+        detect();
+      }, 10)
+    );
+
+    const detect = async () => {
+      // Check if data is available
+      // console.log("videoRef.current", webcamRef.current);
+      if (
+        typeof webcamRef.current !== "undefined" &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState !== null
+      ) {
+        // Get video properties
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+
+        // Set video width and height
+        webcamRef.current.width = videoWidth;
+        webcamRef.current.height = videoHeight;
+
+        // Set canvas width and height
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+
+        // Make detections
+        const detections = await net.detect(video);
+        // console.log(detections);
+
+        // Draw bounding boxes
+        // TODO: Fix mirror bounding box issue
+        const ctx = canvasRef.current.getContext("2d");
+        console.log(videoHeight);
+        if (!(facingMode === "environment" && isMobile)) {
+          ctx.setTransform(1, 0, -1, 0, 0, videoHeight);
+        }
+
+        drawRect(detections, ctx, !(facingMode === "environment" && isMobile));
+      }
+    };
+  };
 
   if (!mediaStream) {
     return <Alert message="No camera detected" type="error" />;
@@ -75,7 +141,7 @@ const Camera = () => {
               mirrored={facingMode === "environment" && isMobile ? false : true}
               ref={webcamRef}
               muted={true}
-              onCanPlay={handleCanPlay}
+              // onCanPlay={handleCanPlay}
               playsInline
               width="100%"
               height="100%"
@@ -92,6 +158,7 @@ const Camera = () => {
               ref={canvasRef}
               width={container.width}
               height={container.height}
+              className="border-5 border-blue-500"
             />
           </Container>
         </Wrapper>

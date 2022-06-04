69b665dda7542ddaa5ec1e6a2ef8df3a494eb91a
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-06-03 18:59:09 -0700

    Enable toggle with the camera button

--- src/components/CameraControls.jsx
+++ src/components/CameraControls.jsx
@@ -1,11 +1,27 @@
 import CameraToggle from "./CameraToggle";
 import PhoneToggle from "./PhoneToggle";
 import { isMobile } from "react-device-detect";
+import { atom, useRecoilState } from "recoil";
+
+export const isCameraOpenState = atom({
+  key: "isCameraOpen",
+  default: false,
+});
 
 const CameraControls = () => {
+  const [isCameraOpen, setIsCameraOpen] = useRecoilState(isCameraOpenState);
+
+  const cameraOpenToggle = () => {
+    setIsCameraOpen(!isCameraOpen);
+    // TODO: Delete for production
+    console.log("Camera toggle clicked. Current state: ", isCameraOpen);
+  };
   return (
     <div className="flex w-2/4 mx-auto">
-      <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
+      <div
+        className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"
+        onClick={() => setIsCameraOpen(!isCameraOpen)}
+      >
         <CameraToggle />
       </div>
       {isMobile && (

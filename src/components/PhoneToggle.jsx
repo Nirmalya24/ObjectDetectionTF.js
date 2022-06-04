6278e251859a8ddc87c38a11543f670358a24606
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-06-04 00:24:57 -0700

    Enable phone camera toggle animation

--- src/components/PhoneToggle.jsx
+++ src/components/PhoneToggle.jsx
@@ -1,12 +1,17 @@
+import { useRecoilValue } from "recoil";
+import { facingModeState } from "./CameraControls";
+
 const PhoneToggle = () => {
+  const facingMode = useRecoilValue(facingModeState);
+
   return (
     <label className="swap swap-active text-6xl">
       {/* Front Facing Camera */}
-      <div className="swap-on">
+      <div className={facingMode === "user" ? "swap-on" : "swap-off"}>
         <i className="bi bi-phone"></i>
       </div>
       {/* Rear facing camera */}
-      <div className="swap-off">
+      <div className={facingMode === "environment" ? "swap-on" : "swap-off"}>
         <i className="bi bi-phone-flip"></i>
       </div>
     </label>
